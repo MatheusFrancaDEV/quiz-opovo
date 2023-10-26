367c521e524af0fecab3620bf386a6a574d8509e
Matheus França <115188878+MatheusFrancaDEV@users.noreply.github.com>
2023-10-26 12:42:44 -0300

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,34 @@
+export default [
+    {
+      question: "Qual significado da sigla I.A",
+      answers: [
+        { option: "Inteligencia Artificial", correct: true },
+        { option: "Inteligencia Atltetica", correct: false },
+        { option: "Interminavel Artificial", correct: false },
+      ],
+    },
+    {
+      question: "Qual o maior produtor de energia eolíca do Brasil",
+      answers: [
+        { option: "Ceará", correct: false },
+        { option: "Rio Grande do Norte", correct: true },
+        { option: "Paraiba", correct: false },
+      ],
+    },
+    {
+      question: "Qual das opções <strong>É uma energia renovavél</strong>",
+      answers: [
+        { option: "Carvão", correct: false },
+        { option: "Gás Natural", correct: false },
+        { option: "Solar", correct: true },
+      ],
+    },
+    {
+      question: "Qual das opções <strong> NÃO É uma energia renovavél</strong>",
+      answers: [
+        { option: "Nuclear", correct: false },
+        { option: "HidroEletrica", correct: true },
+        { option: "Eolica", correct: false },
+      ],
+    },
+  ];
